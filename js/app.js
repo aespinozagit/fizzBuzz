e0d9460bfe0b72b987b08163f22d17bd033e8598
Alex Espinoza <aespinoza123@gmail.com>
2015-07-04 09:23:31 -0500

    FizzBuzz 2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
-	for (var numb = 1; numb <= 100; numb++) {
-		if (numb % 15 === 0){
-		 	$('ul').append('<li>FizzBuzz</li>');
-        }
-    	else if (numb % 3 === 0){
-        	$('ul').append('<li>Fizz</li>');
-    	}
-    	else if (numb % 5 === 0){
-        	$('ul').append('<li>Buzz</li>');
-    	}
-    	else{
-			$('ul').append('<li>' + numb + '</li>');
-		}
-		// console.log(numb);
-	}
 
+	fizzBuzz();
+
+	
+
+    function fizzBuzz() {
+    	var number = prompt("Input number:");
+        var n = parseInt(number);
+        	for (var numb = 1; numb <= n; numb++) {
+				if (numb % 15 === 0){
+		 			$('ul').append('<li>FizzBuzz</li>');
+        		}
+    			else if (numb % 3 === 0){
+        			$('ul').append('<li>Fizz</li>');
+    			}
+    			else if (numb % 5 === 0){
+        			$('ul').append('<li>Buzz</li>');
+    			}
+    			else{
+					$('ul').append('<li>' + numb + '</li>');
+				}
+			}
+    	}
 });
